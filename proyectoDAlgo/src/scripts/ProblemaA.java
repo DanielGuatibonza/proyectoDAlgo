8b42b49e0304e742b85091ade60883e792234816
DanielGuatibonza <dm.guatibonza@uniandes.edu.co>
2019-12-09 19:22:37 -0500

    Problemas A y C

--- proyectoDAlgo/src/scripts/ProblemaA.java
+++ proyectoDAlgo/src/scripts/ProblemaA.java
@@ -1,10 +1,90 @@
 package scripts;
 
-public class ProblemaA {
+// Se importan las clases de librería a utilizar
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+/**
+ * Programa para calcular el tamaño del subarreglo recurrente más largo
+ * @author Daniel Mateo Guatibonza Solano - Juan David Diaz Cristancho 
+ */
+public class ProblemaA 
+{
+	/**
+	 * Método principal de ejecución
+	 * @param args: Arreglo de cadenas de caracteres asociado a los parámetros dados en la ejecución
+	 * @throws Exception: Si ocurre un error al leer el archivo de entrada
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		ProblemaA instanciaA = new ProblemaA();
+		try(InputStreamReader isr= new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(isr)) 
+		{ 
+			String linea = br.readLine();	
+			while(linea!=null && linea.length()>0 && !"0".equals(linea)) 
+			{
+				// Lectura del tamaño del arreglo de entrada
+				int N = Integer.parseInt(linea);
+				// Lectura del arreglo de entrada
+				linea = br.readLine();
+				String[] datosPuntos = linea.split(" ");
+				int[] numeros = Arrays.stream(datosPuntos).mapToInt(f->Integer.parseInt(f)).toArray();
+				// Cálculo del tamaño del subarreglo recurrente más largo y escritura en el archivo de salida
+				System.out.println(instanciaA.subarregloRecurrenteMasLargo(N, numeros));
+				linea = br.readLine();
+			}
+		}
 	}
 
+	/**
+	 * Método para calcular el tamaño del subarreglo recurrente más largo
+	 * @param N: Tamaño del arreglo de entrada
+	 * @param numeros: Arreglo de números sobre el que se desea calcular el subarreglo recurrente más largo
+	 * @return El tamaño del subarreglo recurrente más largo del arreglo numeros
+	 */
+	public int subarregloRecurrenteMasLargo(int N, int[] numeros)
+	{
+		// El algoritmo evalua la posibilidad de que un número sea el inicio de un subarreglo recurrente,
+		// calcula el tamaño del subarreglo recurrente obtenido a partir de ese número y actualiza el subarreglo
+		// más largo obtenido hasta ahora si es conveniente
+		int[] SARML = new int[0];							// Variable usada para almacenar el SubArreglo Recurrente Más Largo
+		boolean noExisteMasLargo = false;
+		for(int i=0; i<N && !noExisteMasLargo; i++)
+		{
+			boolean noExisteMasLargoActual = false;
+			for(int j=i+1; j<N && !noExisteMasLargoActual; j++)
+			{
+				if(numeros[i]==numeros[j])
+				{
+					int[] SARA = new int[0];				// Variable usada para almacenar el SubArreglo Recurrente Actual
+					boolean sigueIgual = true;
+					int k = 0;
+					for(; k<N-j && sigueIgual; k++)
+					{
+						if(numeros[i+k] != numeros[j+k])
+						{
+							sigueIgual = false;
+							k--;
+						}
+					}
+					SARA = Arrays.copyOfRange(numeros, j, j+k);
+					if(SARA.length > SARML.length)
+					{
+						SARML = SARA;
+					}
+				}
+				if(SARML.length >= N-j-1)
+				{
+					noExisteMasLargoActual = true;
+				}
+			}
+			if(SARML.length >= N-i-1)
+			{
+				noExisteMasLargo = true;
+			}
+		}
+		return SARML.length;
+	}
 }
--- proyectoDAlgo/src/scripts/ProblemaC.java
+++ proyectoDAlgo/src/scripts/ProblemaC.java
@@ -1,5 +1,6 @@
 package scripts;
 
+//Se importan las clases de librería a utilizar
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -10,7 +11,11 @@ import java.util.Arrays;
  */
 public class ProblemaC 
 {
-
+	/**
+	 * Método principal de ejecución
+	 * @param args: Arreglo de cadenas de caracteres asociado a los parámetros dados en la ejecución
+	 * @throws Exception: Si ocurre un error al leer el archivo de entrada
+	 */
 	public static void main(String[] args) throws Exception
 	{
 		ProblemaC instanciaC = new ProblemaC();
@@ -33,39 +38,62 @@ public class ProblemaC
 				{
 					poligono.insertarPunto(puntos[i], puntos[i+1]);
 				}
+				// Determinación de la ubicación del punto y escritura en el archivo de salida
 				System.out.println(instanciaC.dentro_o_fuera(poligono, puntoInteres));
+				linea = br.readLine();
 			}
 		}
 	}
 	
+	/**
+	 * Método para calcular si un punto está dentro, fuera o en la frontera de un polígono
+	 * @param poligono: Poligono sobre el que se desea identificar el punto
+	 * @param puntoInteres: Punto sobre el que se quiere determinar su ubicación en el polígono
+	 * @return 0 si el punto está en la frontera, 1 si está adentro, -1 si esta fuera
+	 */
 	public int dentro_o_fuera(Poligono poligono, Punto puntoInteres)
 	{
 		int respuesta = -2;
 		// Se revisa si el punto esta en la frontera del polígono
 		for(int i=0; i<poligono.lados.length && respuesta == -2; i++)
 		{
-			Punto yMayor = null;
-			Punto yMenor = null;
-			if(poligono.lados[i].punto1.y > poligono.lados[i].punto2.y)
+			int yMayor = Math.max(poligono.lados[i].punto1.y, poligono.lados[i].punto2.y);
+			int yMenor = Math.min(poligono.lados[i].punto1.y, poligono.lados[i].punto2.y);
+			// Caso en el que el lado del poligono es vertical
+			if(poligono.lados[i].xConstante)
 			{
-				yMayor = poligono.lados[i].punto1;
-				yMenor = poligono.lados[i].punto2;
+				if(poligono.lados[i].punto1.x == puntoInteres.x && yMenor<=puntoInteres.y && puntoInteres.y<=yMayor)
+				{
+					respuesta = 0;
+				}
 			}
+			// Caso en el que el lado del polígono es horizontal
+			else if(poligono.lados[i].yConstante)
+			{
+				int xMayor = Math.max(poligono.lados[i].punto1.x, poligono.lados[i].punto2.x);
+				int xMenor = Math.min(poligono.lados[i].punto1.x, poligono.lados[i].punto2.x);	
+				if(poligono.lados[i].punto1.y == puntoInteres.y && xMenor<=puntoInteres.x && puntoInteres.x<=xMayor)
+				{
+					respuesta = 0;
+				}
+			}
+			// Caso en el que el lado del polígono tiene pendiente distinta de 0
 			else
 			{
-				yMenor = poligono.lados[i].punto1;
-				yMayor = poligono.lados[i].punto2;
-			}
-			double yEstimado = poligono.lados[i].pendiente * puntoInteres.x + poligono.lados[i].intercepto;
-			if(yEstimado == puntoInteres.y)
-			{
-				
+				float yEstimado = poligono.lados[i].darY(puntoInteres.x);
+				if(yEstimado == puntoInteres.y)
+				{
+					if(yMenor<=yEstimado && yEstimado<=yMayor)
+					{
+						respuesta = 0;
+					}
+				}
 			}
 		}
 		// Si el punto no está en la frontera, se evalua si está adentro o afuera
 		if(respuesta == -2)
 		{
-			// Se revisan las lineas del poligono que están a la derecha del punto de interés
+			// Se identifican las lineas del poligono que están a la derecha del punto de interés
 			boolean[] estanDerecha = new boolean[poligono.lados.length];
 			for(int i=0; i<estanDerecha.length; i++)
 			{
@@ -74,19 +102,65 @@ public class ProblemaC
 					estanDerecha[i] = true;
 				}
 			}
-			// Sobre las lineas a la derecha del punto de interés, se revisa si la proyección horizontal
-			// del punto cruza con algún vértice
-			boolean[] cruzaPorPunto = new boolean[estanDerecha.length];
-			for(int i=0; i<estanDerecha.length; i++)
+			// Se evalua el número de cortes de la proyección horizontal del punto de interés sobre los lados
+			// del polígono que están a la derecha
+			int cruces = 0; int limite = estanDerecha.length;
+			for(int i=0; i<limite; i++)
 			{
-				if((i!= 0 && estanDerecha[i] && estanDerecha[i-1]) || (i==0 && estanDerecha[0] && estanDerecha[estanDerecha.length-1]))
+				// Se evaluan solo las lineas a la derecha que no son horizontales
+				if(estanDerecha[i] && !poligono.lados[i].yConstante)
 				{
-					if(poligono.vertices[i].x == puntoInteres.x)
+					int xMayor = Math.max(poligono.lados[i].punto1.x, poligono.lados[i].punto2.x);
+					int xMenor = Math.min(poligono.lados[i].punto1.x, poligono.lados[i].punto2.x);			
+					float xEstimado = poligono.lados[i].darX(puntoInteres.y);
+					// Caso en el que la proyección del punto se encuentra con el primer vértice agregado
+					if(xEstimado == poligono.lados[i].punto1.x && i==0)
 					{
-						cruzaPorPunto[i] = true;
+						if((poligono.lados[limite-1].punto1.y > puntoInteres.y && poligono.lados[0].punto2.y < puntoInteres.y) || 
+								(poligono.lados[limite-1].punto1.y < puntoInteres.y && poligono.lados[0].punto2.y > puntoInteres.y))
+						{
+							cruces++;
+						}
+						limite--;
+					}
+					// Caso en el que la proyección del punto se encuentra con un vértice distinto al primero
+					else if(xEstimado == poligono.lados[i].punto2.x)
+					{
+						// Se evalua si al cruzar por el vértice la proyección cruza o no el polígono
+						if(i!=estanDerecha.length-1)
+						{
+							if((poligono.lados[i].punto1.y > puntoInteres.y && poligono.lados[i+1].punto2.y < puntoInteres.y) || 
+									(poligono.lados[i].punto1.y < puntoInteres.y && poligono.lados[i+1].punto2.y > puntoInteres.y))
+							{
+								cruces++;
+							}
+							i++;
+						}
+						else
+						{
+							if((poligono.lados[i].punto1.y > puntoInteres.y && poligono.lados[0].punto2.y < puntoInteres.y) || 
+									(poligono.lados[i].punto1.y < puntoInteres.y && poligono.lados[0].punto2.y > puntoInteres.y))
+							{
+								cruces++;
+							}
+						}
+					}
+					// Caso en el que la proyección del punto se encuentra con un lado del polígono
+					else if(xEstimado > puntoInteres.x && xMenor<=xEstimado && xEstimado<=xMayor)
+					{
+						cruces++;
 					}
 				}
-				
+			}
+			// Si el número de cortes es par, el punto está fuera del polígono
+			if(cruces%2 == 0)
+			{
+				respuesta = -1;
+			}
+			// Si el número de cortes es impar, el punto está dentro del polígono
+			else
+			{
+				respuesta = 1;
 			}
 		}
 		return respuesta;
@@ -113,16 +187,57 @@ class Linea
 	// Atributos
 	public Punto punto1;
 	public Punto punto2;
-	public double pendiente;
-	public double intercepto;
+	public float pendiente;
+	public float intercepto;
+	public boolean xConstante;
+	public boolean yConstante;
 	
 	// Método contructor
 	public Linea(Punto p1, Punto p2)
 	{
 		punto1 = p1;
 		punto2 = p2;
-		pendiente = (p1.y - p2.y)/(p1.x - p2.x);
-		intercepto = p1.y - pendiente*p1.x;
+		if(p1.x == p2.x)
+		{
+			xConstante = true;
+		}
+		else if(p1.y == p2.y)
+		{
+			yConstante = true;
+		}
+		if(!xConstante && !yConstante)
+		{
+			pendiente = ((float)p1.y - (float)p2.y)/((float)p1.x - (float)p2.x);
+			intercepto = p1.y - pendiente*p1.x;
+		}
+	}
+	
+	public float darY(int x)
+	{
+		float resultado = -1;
+		if(yConstante)
+		{
+			resultado = punto1.y;
+		}
+		else if(!xConstante)
+		{
+			resultado = pendiente*x + intercepto;
+		}
+		return resultado;
+	}
+	
+	public float darX(int y)
+	{
+		float resultado = -1;
+		if(xConstante)
+		{
+			resultado = punto1.x;
+		}
+		else if(!yConstante)
+		{
+			resultado = (y-intercepto)/pendiente;
+		}
+		return resultado;
 	}
 }
 

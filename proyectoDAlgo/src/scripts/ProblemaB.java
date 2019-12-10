d4ebc4312f8ae827367692a7814351c0e021f027
JUAN DAVID DIAZ CRISTANCHO <jd.diazc@sis.virtual.uniandes.edu.co>
2019-12-09 19:55:01 -0500

    adios

--- proyectoDAlgo/src/scripts/ProblemaB.java
+++ proyectoDAlgo/src/scripts/ProblemaB.java
@@ -3,10 +3,7 @@ package scripts;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Proyecto Final Disenio y Analisis de Algoritmos
